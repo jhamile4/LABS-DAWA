418451e4cf59e5d0f6a2191d8c1c41c9bcf9b8f5
jhamilemacavilca <jhamile.macavilca@tecsup.edu.pe>
2026-03-18 00:53:28 -0500

    otro cambio

---
+++ sem1/app.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fileManager = require("./modules/fileManager");
+const calc = require("./modules/calculadora");
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/notas") {
+        const notas = fileManager.leerNotas();
+        const prom = calc.promedio(notas);
+
+        res.write("Notas: " + JSON.stringify(notas));
+        res.write("\nPromedio: " + prom);
+        res.end();
+    }
+
+    else if (req.url === "/guardar") {
+        const datos = [15, 18, 20, 14];
+        fileManager.guardarNotas(datos);
+
+        res.write("Notas guardadas correctamente");
+        res.end();
+    }
+
+    else {
+        res.write("Ruta no encontrada");
+        res.end();
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Servidor en http://localhost:3000");
+});
