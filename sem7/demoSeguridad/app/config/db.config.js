1a836a590142f52011f76e1abebaedbd4b77957b
jhamilemacavilca <jhamile.macavilca@tecsup.edu.pe>
2026-05-21 22:55:05 -0500

    listo

--- sem7/demoSeguridad/app/config/db.config.js
+++ sem7/demoSeguridad/app/config/db.config.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export default {
   HOST: process.env.DB_HOST,
-  PORT: process.env.DB_PORT || 3306,
+  PORT: process.env.DB_PORT || 5432,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
-  dialect: "mysql"
+  dialect: "postgres"
 };
